0e9b0fd8b99660a94aba809b91d4add1e4febc8f
Matthew Kozlowski <maekozlowski@gmail.com>
2019-04-16 16:30:05 -0600

    created new page for capstone

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+function openFilmSearch(){
+  console.log("openFilmSearch ran.")
+  $('.links').on('click', function(){
+    $('main').html(`<h1>It Work!</h1>`);
+  })
+}
+/*
 twttr.widgets.createTimeline(
     {
       sourceType: "profile",
       screenName: "LanternCinema"
     },
-    document.getElementById("container")
+    document.getElementById("twitter-timeline")
   );
+*/
+openFilmSearch();
+  
+  
